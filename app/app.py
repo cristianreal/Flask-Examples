b796871523321ca2d57e021736f7d4113318aea6
Christian Real <201513700@ingenieria.usac.edu.gt>
2019-10-18 02:43:04 -0600

    v1.0.20
    fix

--- app/app.py
+++ app/app.py
@@ -17,13 +17,13 @@ def hello_world():
 def Prueba():
 	if request.method == 'POST':
 		data = usuario = [
-			{'nombre': 'Alejandro'},
-			{'nombre': 'Miguel'},
-			{'nombre': 'Mario'},
-			{'nombre': 'Osmary'},
-			{'nombre': 'Dachi'},
-			{'nombre': 'Luis'},
-			{'nombre': 'Aldo'}
+			{'id': '12340', 'name': 'Alejandro', 'path': 'es_gt', 'languaje':'es', 'state':'completo', 'user': 'Happy1'},
+			{'id': '12345', 'nombre': 'Miguel',  'path': 'en_eu', 'languaje':'en', 'state':'incompleto', 'user': 'Happy2'},
+			{'id': '12346', 'nombre': 'Mario',   'path': 'en_uk', 'languaje':'uk', 'state':'completo', 'user': 'Happy3'},
+			{'id': '12347', 'nombre': 'Osmary',  'path': 'es_ar', 'languaje':'ar', 'state':'incompleto', 'user': 'Happy4'},
+			{'id': '12348', 'nombre': 'Dachi',   'path': 'es_es', 'languaje':'es', 'state':'completo', 'user': 'Happy5'},
+			{'id': '12349', 'nombre': 'Luis',    'path': 'fr_fr', 'languaje':'fr', 'state':'incompleto', 'user': 'Happy6'},
+			{'id': '12341', 'nombre': 'Aldo',    'path': 'es_it', 'languaje':'it', 'state':'completo', 'user': 'Happy7'}
 		]
 		#headers = {'Content-type': 'application/json'}
 		return render_template('index.html', data=data)
